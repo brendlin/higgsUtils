0efc11c17fca451e66e5621682fb4b7d4ad2bfc8
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2017-05-10 10:49:44 +0200

    Slightly fix integral of BkgReweighting (From Apr 23)

--- macros/BackgroundReweighting.py
+++ macros/BackgroundReweighting.py
@@ -11,45 +11,8 @@ offset = 0
 
 plotfunc.SetupStyle()
 
-categories = [
-    'Inclusive',
-    'M17_ggH_0J_Cen',        # 1
-    'M17_ggH_0J_Fwd',        # 2
-    'M17_ggH_1J_LOW',        # 3
-    'M17_ggH_1J_MED',        # 4
-    'M17_ggH_1J_HIGH',       # 5
-    'M17_ggH_1J_BSM',        # 6
-    'M17_ggH_2J_LOW',        # 7
-    'M17_ggH_2J_MED',        # 8
-    'M17_ggH_2J_HIGH',       # 9
-    'M17_ggH_2J_BSM',        # 10
-    'M17_VBF_HjjLOW_loose',  # 11
-    'M17_VBF_HjjLOW_tight',  # 12
-    'M17_VBF_HjjHIGH_loose', # 13
-    'M17_VBF_HjjHIGH_tight', # 14
-    'M17_VHhad_loose',       # 15
-    'M17_VHhad_tight',       # 16
-    'M17_qqH_BSM',           # 17
-    'M17_VHMET_LOW',         # 18
-    'M17_VHMET_HIGH',        # 19
-    'M17_VHMET_BSM',         # 20
-    'M17_VHlep_LOW',         # 21
-    'M17_VHlep_HIGH',        # 22
-    'M17_VHdilep_LOW',       # 23
-    'M17_VHdilep_HIGH',      # 24
-#     'M17_ttH_Had_6j2b',      # 25
-#     'M17_ttH_Had_6j1b',      # 26
-#     'M17_ttH_Had_5j2b',      # 27
-#     'M17_ttH_Had_5j1b',      # 28
-#     'M17_ttH_Had_4j2b',      # 29
-#     'M17_ttH_Had_4j1b',      # 30
-#     'M17_ttH_Lep',           # 31
-#     'M17_ttH_Lep_0fwd',      # 32
-#     'M17_ttH_Lep_1fwd',      # 33
-    ]
-
 def integral(hist,f,l) :
-    return hist.Integral(hist.FindBin(f+0.00000001),hist.FindBin(l+0.00000001))
+    return hist.Integral(hist.FindBin(f+0.000001),hist.FindBin(l-0.000001))
 
 
 def DoRescaleProcedure(af2,bkg,name,ci,c) :
@@ -128,7 +91,11 @@ def main(options,args) :
     outfile = ROOT.TFile('out.root','RECREATE')
 
     cans = []
-    for i,c in enumerate(categories) :
+    for i,c in enumerate(['Inclusive']+Tools.categories) :
+
+        if c == 'M17_VHMET_LOW' :
+            break
+
         #print 'HGamEventInfoAuxDyn_m_yy_over_1000_c%d_%s_AF2'%(i+offset,c)
         tmp = []
 
